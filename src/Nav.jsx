65f7f6a07f4c559e8d91c75d034bd6f4a303fd04
Nick Jonescu <nicholasjonescu@gmail.com>
2022-12-28 16:13:58 -0500

    starting home page

---
+++ src/Nav.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div className='nav-container'>
+      <div className='contact-info'>
+        <p className='address'>235 Dixon Road Unit 106, Etobicoke, ON, M9P 2M5 Canada</p>
+        <p className='phone-number'>+1 (800) 799 5491</p>
+      </div>
+      <div className='social-icons'>
+        <ul>
+          <li>insta</li>
+          <li>fb</li>
+          <li>linkedin</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Nav
